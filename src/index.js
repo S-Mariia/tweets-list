fb268858459923b08b35e95e737bfb963d761d49
S-Mariia <savchyn.mar@gmail.com>
2023-06-05 12:19:20 +0300

    prepare for deploying to netlify.com

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { App } from 'App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/tweets-list">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
